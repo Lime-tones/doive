6f6ed5fffa0c89e0d702ecb8bb53c91469fd8945
Lime-tones <135816605+Lime-tones@users.noreply.github.com>
2024-05-14 23:38:05 +0600

    Update env.py

--- env.py
+++ env.py
@@ -1,12 +1,5 @@
 import os, sys, IPython
+
 def install_vendors():
-    
-    print("Installing vendors -> ", end="")
-    import os, IPython
-    if os.path.exists("vendor/.installed"):
-        print("Done.")
-        return
-    try:
-       print("Done.")
-    except Exception as e:
-        print("Error: %s" % e)
+    # Function content is here
+    pass
